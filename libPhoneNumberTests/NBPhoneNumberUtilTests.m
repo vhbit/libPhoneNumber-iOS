8012ce6493e33bcd04bdd7f2ce643895262d352f
ishtar <ishtar@ishtars-iMac.local>
2013-09-22 16:57:42 +0900

    Add UnitTest

--- libPhoneNumberTests/NBPhoneNumberUtilTests.m
+++ libPhoneNumberTests/NBPhoneNumberUtilTests.m
@@ -103,6 +103,25 @@
     STAssertEqualObjects(nationalNumberForExpect, nationalNumberForActual, nil);
 }
 
+- (void)testForiOS7
+{
+    NBPhoneNumberUtil *phoneUtil = [NBPhoneNumberUtil sharedInstance];
+    
+    NSError *aError = nil;
+    NBPhoneNumber *myNumber = [phoneUtil parse:@"0174 2340XXX" defaultRegion:@"DE" error:&aError];
+    if (aError == nil)
+    {
+        NSLog(@"isValidPhoneNumber ? [%@]", [phoneUtil isValidNumber:myNumber] ? @"YES":@"NO");
+        NSLog(@"E164          : %@", [phoneUtil format:myNumber numberFormat:NBEPhoneNumberFormatE164 error:&aError]);
+        NSLog(@"INTERNATIONAL : %@", [phoneUtil format:myNumber numberFormat:NBEPhoneNumberFormatINTERNATIONAL error:&aError]);
+        NSLog(@"NATIONAL      : %@", [phoneUtil format:myNumber numberFormat:NBEPhoneNumberFormatNATIONAL error:&aError]);
+        NSLog(@"RFC3966       : %@", [phoneUtil format:myNumber numberFormat:NBEPhoneNumberFormatRFC3966 error:&aError]);
+    }
+    else
+    {
+        NSLog(@"Error : %@", [aError localizedDescription]);
+    }
+}
 
 - (void)testNSDictionaryalbeKey
 {
