48bda9b893fa8a413affebcb4d98d8a399b3e097
Valerii Hiora <valerii.hiora@gmail.com>
2013-11-06 16:57:14 +0200

    Framework-related changes
    - installation directory is set to embed
    - framework is renamed to PhoneNumber
    - added a general header file to avoid one-by-one inclusion

---
+++ libPhoneNumber/PhoneNumber.h
@@ -0,0 +1,19 @@
+//
+//  PhoneNumber.h
+//  libPhoneNumber
+//
+//  Created by Valerii Hiora on 11/6/13.
+//  Copyright (c) 2013 NHN. All rights reserved.
+//
+
+#ifndef libPhoneNumber_PhoneNumber_h
+#define libPhoneNumber_PhoneNumber_h
+
+#import "PhoneNumber/NBPhoneNumber.h"
+#import "PhoneNumber/NBPhoneMetaData.h"
+#import "PhoneNumber/NBNumberFormat.h"
+#import "PhoneNumber/NBPhoneNumberDesc.h"
+#import "PhoneNumber/NBPhoneNumberUtil.h"
+#import "PhoneNumber/NBAsYouTypeFormatter.h"
+
+#endif
